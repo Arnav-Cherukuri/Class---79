6ca5c1656c97e6cbc3aa8854c27a02c07d709a22
Arnav-Cherukuri <73111342+Arnav-Cherukuri@users.noreply.github.com>
2020-12-05 16:43:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+student_array = [];
+function submit(){
+var name_1 = document.getElementById("nots1").value;
+var name_2 = document.getElementById("nots2").value;
+var name_3 = document.getElementById("nots3").value;
+var name_4 = document.getElementById("nots4").value;
+
+student_array.push(name_1);
+student_array.push(name_2);
+student_array.push(name_3);
+student_array.push(name_4);
+console.log(student_array);
+document.getElementById("display_name").innerHTML=student_array;
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display="inline-block";
+};
+function sort(){
+ student_array.sort();
+ console.log(student_array);
+ document.getElementById("display_name").innerHTML=student_array;
+};
